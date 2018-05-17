a9c9104c20f59f8e56a2f6064216b8b1960298e1
Joseph Casey <joseph@locuslabs.com>
2018-05-17 15:41:08 -0400

    🐛 FIX(timeout): Provided a more reasonable timeout of 15 for unzipping files instead of 3 seconds. 📚 DOCS(sam-cli): Better explained a prerequisites for sam cli invocations. 👌 REFACTOR(try/catch): Moved current function logic into a single function for simplicity.

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Project Based on https://github.com/lifeomic/lambda-typescript-webpack-babel-sta
 
 # Sample SAM CLI Invocations
 
+Warning SAM CLI Invocations are based on the bundled distribution. Be sure the dist/ is bundled before making local sam cli invocations.
+
 sam local invoke "HelloWorld" -e event/hello-world.json  -t sam.yaml --debug
-sam local invoke "Unzip" -e event/unzip.json  -t sam.yaml  --log-file ./output.log
+sam local invoke "Unzip" -e event/unzip.json  -t sam.yaml  --log-file ./output.log --profile josephmcasey --debug
--- src/lambdas/unzip/index.ts
+++ src/lambdas/unzip/index.ts
@@ -1,47 +1,38 @@
-// import { createInflateRaw } from 'zlib'
+import { createInflateRaw } from 'zlib'
 import * as AWS from 'aws-sdk'
 const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: 'us-west-2' })
 
-async function run (event: any, context: AWSLambda.Context) {
+export async function handler (event: any, context: AWSLambda.Context, callback: AWSLambda.Callback) {
 
-  console.log('Entering Unzip#Run')
+  try {
+    console.log('Entering Unzip#Run')
 
-  const s3OutputBucket = event['CodePipeline.job'].data.actionConfiguration.configuration.UserParameters
-  const s3Input = event['CodePipeline.job'].data.inputArtifacts[0].location.s3Location
-  const s3InputBucket = s3Input.bucketName
-  const s3InputKey = s3Input.objectKey
+    const s3OutputBucket = event['CodePipeline.job'].data.actionConfiguration.configuration.UserParameters
+    const s3Input = event['CodePipeline.job'].data.inputArtifacts[0].location.s3Location
+    const s3InputBucket = s3Input.bucketName
+    const s3InputKey = s3Input.objectKey
 
-  console.log(`
+    console.log(`
     S3 Output Bucket: ${s3OutputBucket}
     S3 Input Bucket: ${s3InputBucket}
     S3 Input Key: ${s3InputKey}
   `)
 
-  await s3.waitFor('bucketExists', { Bucket: s3OutputBucket }).promise()
-  await s3.waitFor('bucketExists', { Bucket: s3InputBucket }).promise()
-  await s3.waitFor('objectExists', { Bucket: s3InputBucket, Key: s3InputKey }).promise()
+    await s3.waitFor('bucketExists', { Bucket: s3OutputBucket }).promise()
+    await s3.waitFor('bucketExists', { Bucket: s3InputBucket }).promise()
+    await s3.waitFor('objectExists', { Bucket: s3InputBucket, Key: s3InputKey }).promise()
 
-  // TODO: Figure out how to unzip the contents of a zipped file and upload that into an s3 bucket.
-  let readStream = s3.getObject({ Bucket: s3InputBucket, Key: s3InputKey }) // .createReadStream().pipe(createInflateRaw())
+      // TODO: Figure out how to unzip the contents of a zipped file and upload that into an s3 bucket.
+    let readStream = s3.getObject({ Bucket: s3InputBucket, Key: s3InputKey }).createReadStream().pipe(createInflateRaw())
 
-  console.log(readStream)
+      // console.log(readStream)
 
-  // let data = await s3.upload({ Bucket: s3OutputBucket, Key: 'some_key', Body: readStream }).promise()
+    let data = await s3.upload({ Bucket: s3OutputBucket, Key: 'some_key', Body: readStream }).promise()
 
-  return null // data
+    callback(null, data)
 
-}
-
-export function handler (event: any, context: AWSLambda.Context, callback: AWSLambda.Callback) {
-
-  console.log('Hello World?')
-
-  run(event, context)
-    .then((result: any) => {
-      callback(null, result)
-    })
-    .catch((err) => {
-      console.error(err, 'Error occurred')
-      callback(err)
-    })
+  } catch (err) {
+    console.error(err, 'Error occurred')
+    callback(err)
+  }
 }
