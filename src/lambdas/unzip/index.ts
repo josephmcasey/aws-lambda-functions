32efa6aa81ec8e9871447b487457ffa31cef6c74
Joseph Casey <joseph@locuslabs.com>
2018-05-18 12:20:28 -0400

    👍 FEAT(unzip): Created a functional unzip that takes an s3 zip file containing a flat directory and provides the correct mime-types to a designated s3 bucket. 🐛 FIX(sam): Added a 60s timeout to avoid timeouts during the unzipping and upload phases. 🎁 DEPENDENCY(unzip): Added JSZip and Mime-Types

--- src/lambdas/unzip/index.ts
+++ src/lambdas/unzip/index.ts
@@ -1,5 +1,7 @@
-import { createInflateRaw } from 'zlib'
 import * as AWS from 'aws-sdk'
+// import mime from 'mime-types'
+import * as JSZip from 'jszip'
+import * as mime from 'mime-types'
 const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: 'us-west-2' })
 
 export async function handler (event: any, context: AWSLambda.Context, callback: AWSLambda.Callback) {
@@ -22,14 +24,31 @@ export async function handler (event: any, context: AWSLambda.Context, callback:
     await s3.waitFor('bucketExists', { Bucket: s3InputBucket }).promise()
     await s3.waitFor('objectExists', { Bucket: s3InputBucket, Key: s3InputKey }).promise()
 
-      // TODO: Figure out how to unzip the contents of a zipped file and upload that into an s3 bucket.
-    let readStream = s3.getObject({ Bucket: s3InputBucket, Key: s3InputKey }).createReadStream().pipe(createInflateRaw())
+    const s3GetResponse: any = await s3.getObject({ Bucket: s3InputBucket, Key: s3InputKey }).promise()
+    const zipContentBuffer: Buffer = s3GetResponse.Body
 
-      // console.log(readStream)
+    const zip: JSZip = await new JSZip().loadAsync(zipContentBuffer)
 
-    let data = await s3.upload({ Bucket: s3OutputBucket, Key: 'some_key', Body: readStream }).promise()
+    let fileMap = Object.entries(zip.files)
 
-    callback(null, data)
+    let uploadResults = []
+
+    // TODO: Handle dir in Zip Objects - https://stuk.github.io/jszip/documentation/api_zipobject.html
+    for (let [key, file] of fileMap) {
+      console.log('File: ', key, file)
+
+      const mimetype = mime.lookup(file.name) || 'application/octet-stream'
+      const s3Param = {
+        Bucket: s3OutputBucket,
+        Key: file.name,
+        Body: await file.async('nodebuffer'),
+        ContentType: mimetype
+      }
+
+      uploadResults.push(await s3.upload(s3Param).promise())
+    }
+
+    callback(null, uploadResults)
 
   } catch (err) {
     console.error(err, 'Error occurred')
